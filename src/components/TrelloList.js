b3f4f8021167893626c34db9bcbbe3b06f63a9b6
Your Name <you@example.com>
2023-02-08 00:00:52 +0100

    TrelloList

---
+++ src/components/TrelloList.js
@@ -0,0 +1,12 @@
+import { Paper, CssBaseline } from "@material-ui/core";
+
+const TrelloList = () => {
+  return (
+    <Paper>
+        <CssBaseline></CssBaseline>
+        <h1>mesto si</h1>
+    </Paper>
+  )
+}
+
+export default TrelloList
