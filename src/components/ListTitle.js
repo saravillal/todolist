a6f479b41c7dec8412253f16c8f8b6d964cff758
Your Name <you@example.com>
2023-02-19 00:19:22 +0100

    tituloListas

--- src/components/ListTitle.js
+++ src/components/ListTitle.js
@@ -1,16 +1,32 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { InputBase, makeStyles, Typography } from "@material-ui/core";
+import { useState } from "react";
 
-const ListTitle = () => {
+const ListTitle = ({title, listId}) => {
   const classes = useStyle();
+  const[open, setOpen] = useState(true)
+  const [newTitle, setNewTitle] = useState(title)
   return (
+    <>
+    { open ? (
+      <InputBase
+      value={newTitle}
+      onChange={e=>setNewTitle(e.target.value)}
+      autoFocus
+      fullWidth
+      inputProps={{className: classes.input}}
+      />
+    ) : (
     <div className={classes.title}>
     <Typography className={classes.titleText}>
-      To do
+      {title}
     </Typography>
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
   <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
 </svg>
     </div>
+    )}
+    </>
+    
     
   )
 }
@@ -24,6 +40,14 @@ const useStyle = makeStyles(theme => ({
     flexGrow: 1,
     fontSize: "1.2rem",
     fontWeigth: "bold"
+  },
+  input: {
+    fontSize: "1.2rem",
+    fontWeight: "bold",
+    margin: theme.spacing(1),
+    "&:focus": {
+      background: "#ddd"
+    }
   }
   }))
 
